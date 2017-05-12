a5275e977545de979374ebf1157969148875a9bc
chengshiliang <HeT@chengsl-2.local>
2017-05-12 16:57:09 +0800

    change podfile

---
+++ QRCodeTest/Pods/Headers/Private/QRCodeMaster/QRCodeScanView.h
@@ -0,0 +1,1 @@
+../../../../../QRCodeLibrary/QRCodeScanView.h
---
+++ QRCodeTest/Pods/Target Support Files/QRCodeMaster/QRCodeMaster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QRCodeMaster : NSObject
+@end
+@implementation PodsDummy_QRCodeMaster
+@end
